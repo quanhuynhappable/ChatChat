daa15c96de1299df76a512c8e0d5b1ef86ac807e
quanhuynhappable <quanhuynh@goappable.com>
2016-05-05 17:01:13 +0700

    3rd commit

---
+++ ChatChat/ChatChat/ViewControllers/ChatViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ChatViewController.swift
+//  ChatChat
+//
+//  Created by Appable on 5/5/16.
+//  Copyright © 2016 Appable. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import JSQMessagesViewController
+class ChatViewController: JSQMessagesViewController {
+    
+    // MARK: Properties
+    var messages = [JSQMessage]()
+    var outgoingBubbleImageView: JSQMessagesBubbleImage!
+    var incomingBubbleImageView: JSQMessagesBubbleImage!
+    
+    // MARK: UIViewController Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupBubbles()
+        collectionView!.collectionViewLayout.incomingAvatarViewSize = CGSizeZero
+        collectionView!.collectionViewLayout.outgoingAvatarViewSize = CGSizeZero
+    }
+    
+    // MARK: CollectionView Methods
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = messages[indexPath.item]
+        if message.senderId == senderId {
+            return outgoingBubbleImageView
+        } else {
+            return incomingBubbleImageView
+        }
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    // MARK: Private Methods
+    private func setupBubbles() {
+        let factory = JSQMessagesBubbleImageFactory()
+        outgoingBubbleImageView = factory.outgoingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleRedColor())
+        incomingBubbleImageView = factory.incomingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleGreenColor())
+    }
+    
+    func addMessage(id: String, text: String) {
+        let message = JSQMessage(senderId: id, displayName: "", text: text)
+        messages.append(message)
+    }
+}
---
+++ ChatChat/ChatChat/ViewControllers/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  ChatChat
+//
+//  Created by Appable on 5/5/16.
+//  Copyright © 2016 Appable. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+class LoginViewController: UIViewController {
+    
+    // MARK: Properties
+    var ref = Firebase(url: "https://demochatchat.firebaseio.com")
+    
+    // MARK: UIViewController LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: Actions
+    @IBAction func loginDidTouch(sender: AnyObject) {
+        ref.authAnonymouslyWithCompletionBlock { (error, authData) in
+            if error != nil {
+                print(error.description)
+                return
+            }
+            self.performSegueWithIdentifier("LoginToChat", sender: nil)
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        super.prepareForSegue(segue, sender: sender)
+        let navViewController = segue.destinationViewController as! UINavigationController
+        let chatViewController = navViewController.viewControllers.first as! ChatViewController
+        chatViewController.senderId = ref.authData.uid
+        chatViewController.senderDisplayName = ""
+    }
+}
